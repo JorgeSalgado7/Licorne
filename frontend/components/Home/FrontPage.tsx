b81d5f56638ed8313c28b3df43835cf75dfbdd38
MariaMeza03 <mmeza@licorne.mx>
2022-07-19 10:19:02 -0500

    modification

--- frontend/components/Home/FrontPage.tsx
+++ frontend/components/Home/FrontPage.tsx
@@ -27,17 +27,7 @@ const frontPage = () => {
 
 				</div>
 
-				<div className='front_page__mobile'>
-
-					<div className='container'>
-						<div className='front_page__mobile__logo'>
-							<div className='front_page__mobile__logo__image_logo'>
-								<img src='https://licorne-web.s3.us-east-2.amazonaws.com/logo/logo-movile.svg' alt='tecnologia' />
-							</div>
-						</div>
-					</div>
-
-				</div>
+			
 		
 		</div>
 
