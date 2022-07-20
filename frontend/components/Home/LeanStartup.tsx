5a01a71e75086b38c60554bf259b81437c847153
MariaMeza03 <mmeza@licorne.mx>
2022-07-20 12:54:51 -0500

    LICORNE-182

---
+++ frontend/components/Home/LeanStartup.tsx
@@ -0,0 +1,145 @@
+///* React components
+import { useLayoutEffect } from 'react'
+import React from "react"
+
+//*Animation
+import { gsap } from "gsap/dist/gsap"
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger"
+
+gsap.registerPlugin(ScrollTrigger);
+
+//*Components
+import Titulo from '../Text/Title'
+
+/**
+ * @author Maria Meza
+ * @name StartKings
+ * @description component StartupKing
+ */
+
+ const StartKings = () => {
+
+	useLayoutEffect(() => {
+		
+		/**
+		 * * @ANIMACIÓN_2
+		 * * CARITA LEAN STARTUP
+		*/
+		const ANIMACION_LEAN_STARTUP = gsap.timeline({
+			scrollTrigger: {
+				trigger: '.lean_startup',
+				markers: false,
+				start: 'top',
+				end: '200%',
+				scrub: true,
+				pin: true,
+			}
+		})
+
+
+		ScrollTrigger.matchMedia({
+
+			'(max-width: 1200px)': function () {
+
+				ANIMACION_LEAN_STARTUP
+				.from('.lean_startup__animacion__ojoLean', { top: '-100%', left: '-30%', opacity: '0', duration: 1 }, '-=1')
+				.to('.lean_startup__animacion__ojoLean', { top: '0', left: '-30%', opacity: '1', duration: 1 }, '-=1')
+				
+				.from('.lean_startup__animacion__ojoGuiñoLean', { top: '-100%', left: '20%', opacity: '0', duration: 1 }, '-=2')
+				.to('.lean_startup__animacion__ojoGuiñoLean', { top: '0', left: '20%', opacity: '1', duration: 1 }, '-=2')
+
+				.from('.lean_startup__animacion__bocaLean', { top: '-100%', left: '35%', opacity: '0', duration: 1 }, '-=3')
+				.to('.lean_startup__animacion__bocaLean', { top: '50%', left: '15%', opacity: '1', duration: 1 }, '-=3')
+
+			
+				
+				
+				.to('.lean_startup__animacion__ojoLean', { height: '80px', transform: 'rotate(45deg)', top: '10%', opacity: '1'  }, '+=1')
+				.to('.lean_startup__animacion__bocaLean', { left: '-30%', top: '70%', transform: 'rotate(225deg)', opacity: '1' })
+				.to('.lean_startup__animacion__ojoGuiñoLean', { height: '30px', width: '80px', transform: 'rotate(-45deg)', top: '25%' , opacity: '1' }, '+=1')
+				.to('.lean_startup__animacion__mosca', { opacity: '1', top: '-50%', left: '15%' }, '+=1')
+				
+				.from('.lean_startup__animacion__ojoDosLean', { opacity: '0' }, '+=1')
+				.to('.lean_startup__animacion__ojoDosLean', { opacity: '1', left: '-30%', top:'10%'}, '+=1')
+				.from('.lean_startup__animacion__ojoGuiñoDosLean', { opacity: '0' }, '+=1')
+				.to('.lean_startup__animacion__ojoGuiñoDosLean', { opacity: '1', left: '35%', top:'10%'  }, '+=1')
+
+				.to('.lean_startup__animacion__bocaLean', { left: '-30%', top: '70%', transform: 'rotate(225deg)', opacity: '1' })
+				},
+
+			'(min-width: 1199px)': function () {
+
+				ANIMACION_LEAN_STARTUP
+					.from('.lean_startup__animacion__ojoLean', { top: '-100%', left: '-30%', opacity: '0', duration: 1 }, '-=1')
+					.to('.lean_startup__animacion__ojoLean', { top: '0', left: '-30%', opacity: '1', duration: 1 }, '-=1')
+					
+					.from('.lean_startup__animacion__ojoGuiñoLean', { top: '-100%', left: '20%', opacity: '0', duration: 1 }, '-=2')
+					.to('.lean_startup__animacion__ojoGuiñoLean', { top: '0', left: '20%', opacity: '1', duration: 1 }, '-=2')
+
+					.from('.lean_startup__animacion__bocaLean', { top: '-100%', left: '35%', opacity: '0', duration: 1 }, '-=3')
+					.to('.lean_startup__animacion__bocaLean', { top: '50%', left: '15%', opacity: '1', duration: 1 }, '-=3')
+
+					.from('.lean_startup__animacion__mosca', { opacity: '0' }, '+=1')
+					.to('.lean_startup__animacion__mosca', { opacity: '1', top: '-50%', left: '15%' }, '+=1')
+					
+					.to('.lean_startup__animacion__ojoLean', { height: '160px', transform: 'rotate(45deg)', top: '10%', opacity: '1'  }, '+=1')
+					.to('.lean_startup__animacion__bocaLean', { left: '-30%', top: '70%', transform: 'rotate(225deg)', opacity: '1' })
+					.to('.lean_startup__animacion__ojoGuiñoLean', { height: '60px', width: '160px', transform: 'rotate(-45deg)', top: '25%' , opacity: '1' }, '+=1')
+					
+					
+					.from('.lean_startup__animacion__ojoDosLean', { opacity: '0' }, '+=1')
+					.to('.lean_startup__animacion__ojoDosLean', { opacity: '1', left: '-30%' }, '+=1')
+					.from('.lean_startup__animacion__ojoGuiñoDosLean', { opacity: '0' }, '+=1')
+					.to('.lean_startup__animacion__ojoGuiñoDosLean', { opacity: '1', left: '35%'  }, '+=1')
+
+					.to('.lean_startup__animacion__bocaLean', { left: '-30%', top: '70%', transform: 'rotate(225deg)', opacity: '1' })
+				
+
+				},
+
+		})
+
+		return () => {
+			
+		
+		  };
+
+		}, [])
+
+	return (
+
+		<div className='lean_startup'>
+
+			<div className='container'>
+
+				<Titulo
+					alternativo="LA DIFERENCIA ENTRE"
+					superior="Escalar o morir se llama"
+					inferior="Lean Startup"
+					invertido={false}
+				/>
+
+				<div className='lean_startup__animacion'>
+					<div className='lean_startup__animacion__mosca'>
+						<img src="https://licorne-web.s3.us-east-2.amazonaws.com/home/moscas.svg" alt="marketing digital"/>
+					</div>
+
+					<div className="lean_startup__animacion__ojoLean"></div>
+					<div className="lean_startup__animacion__ojoDosLean"></div>
+					<div className="lean_startup__animacion__ojoGuiñoLean"></div>
+					<div className="lean_startup__animacion__ojoGuiñoDosLean"></div>
+					<div className="lean_startup__animacion__bocaLean"></div>
+				</div>
+
+				{/* <LinkPersonalizado
+					url="/asi-lo-hacemos"
+					texto="¿CÓMO LO HACEN?"
+				/> */}
+
+			</div>
+
+		</div>
+	)
+}
+
+export default StartKings 
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,17 @@
 //* Next components
 import type { NextPage } from 'next'
 
+
 //* Components
 import MainLayout from '../layouts/MainLayout'
 import FrontPage from '../components/Home/FrontPage'
 import StartKings from '../components/Home/StartupKings'
+import LeanStartup from '../components/Home/LeanStartup'
+
+//*Animation
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger)
 
 /**
  * @author Jorge Salgado
@@ -17,6 +24,8 @@ const Home: NextPage = () => {
 	
 	const title = 'Metodología Lean Startup aplicada a consolidar tu negocio'
 	const description = 'En Licorne Startup kings damos vida a ideas de negocio para consolidar marcas mediante consultoría, tecnología y marketing digital. ¡Iniciemos hoy mismo!'
+	
+
 
 	return (
 
@@ -28,10 +37,12 @@ const Home: NextPage = () => {
 				
 				<StartKings/>
 
-			</div>
+				<LeanStartup/>
 
+			</div>
 		
-			
+		
+
 		</MainLayout>
 
 	)
