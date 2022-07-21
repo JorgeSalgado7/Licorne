4501897ebcad11d3dfd382e00857a8249b91904f
MariaMeza03 <mmeza@licorne.mx>
2022-07-21 18:19:16 -0500

    modification

--- frontend/components/Home/SlideHome.tsx
+++ frontend/components/Home/SlideHome.tsx
@@ -28,22 +28,23 @@ const slide: NextPage = () => {
 
 				const ANIMACION_SLIDE = gsap.timeline({
 					scrollTrigger: {
-						trigger:  animation.current,
+						trigger: animation.current,
 						markers: false,
+						refresh: true,
 						start: 'top',
 						end: '600%',
 						scrub: true,
 						pin: true,
-						}
-					})
+					}
+				})
 
-					ANIMACION_SLIDE
-						.to('.slide_home__slide__pag2', { left: '50%', duration: 0 }, '+=6')
-						.to('.slide_home__slide__pag3', { left: '50%', duration: 0 }, '+=6')
-						.to('.slide_home__slide__pag4', { left: '50%', duration: 0 }, '+=6')
-						.to('.slide_home__slide__pag5', { left: '50%', duration: 0 }, '+=6')
-						.to('.slide_home__slide__cubo', { top: '100%' }, '+=2')
-					},
+				ANIMACION_SLIDE
+					.to('.slide_home__slide__pag2', { left: '50%', duration: 0 }, '+=6')
+					.to('.slide_home__slide__pag3', { left: '50%', duration: 0 }, '+=6')
+					.to('.slide_home__slide__pag4', { left: '50%', duration: 0 }, '+=6')
+					.to('.slide_home__slide__pag5', { left: '50%', duration: 0 }, '+=6')
+					.to('.slide_home__slide__cubo', { top: '100%' }, '+=2')
+			},
 
 			'(min-width: 1199px)': function () {
 
@@ -51,6 +52,7 @@ const slide: NextPage = () => {
 					scrollTrigger: {
 						trigger: animation.current,
 						markers: false,
+						refresh: true,
 						start: 'top',
 						end: '200%',
 						scrub: true,
@@ -59,11 +61,8 @@ const slide: NextPage = () => {
 				})
 
 				ANIMACION_SLIDE
-
-					.to('.slide_home__slide__pag4', { left: '50%', duration: 0 , opacity: 0 }, '+=6')
 					.to('.slide_home__slide__pag2', { left: '50%', duration: 0 }, '+=6')
-					.to('.slide_home__slide__pag3', { left: '50%', duration: 0 }, '+=6')
-				
+					.to('.slide_home__slide__pag4', { left: '50%', duration: 0 }, '+=6')
 					.to('.slide_home__slide__cubo', { top: '100%' }, '+=6')
 
 			},
@@ -75,14 +74,14 @@ const slide: NextPage = () => {
 
 	return (
 
-		<div className='slide_home' ref={animation}>
-			
-			<div className='slide_home__slide'>
+		<div className='slide_home'>
+			{/* SLIDE */}
+			<div className='slide_home__slide' ref={animation}>
 
 				<div className='slide_home__slide__pag1'>
-					<div className='container'>
+					<div className='contenedor container'>
 
-						<div className='slide_home__slide__pag1__text'>
+						<div className='contenedor__text_pag1'>
 							<h3 className="tir">¡NUESTRO MANIFIESTO!</h3>
 							<p className='size'>&quot;9 de cada 10 startups mueren antes de los 3 años… <span>¡Licorne® Startup Kings rompe esa estadística! </span>
 				Ayudando a los creadores de un mejor futuro a transformar su idea en una compañía rentable, a través de consultoría
@@ -103,28 +102,28 @@ const slide: NextPage = () => {
 				</div>
 
 				<div className='slide_home__slide__pag2'>
-					<div className='container'>
+					<div className='contenedor container'>
 
-						<div className='slide_home__slide__pag2__text'>
+						<div className='contenedor__text_pag2'>
 							<h3>CONSULTORÍA LEAN STARTUP</h3>
 							<p>Los cracks en lean startup, estrategia de negocio y por supuesto, digital ¡están aquí! </p>
 						</div>
 
-						<div className='slide_home__slide__pag2__textDos'>
+						<div className='contenedor__textDos_pag2'>
 							<h3>TECNOLOGÍA</h3>
 							<p>¿Buscas desarrollo de aplicaciones, ecommerce o sitios web para tu MVP? Estás en el sitio correcto a la hora exacta.</p>
 						</div>
 
-						<div className='slide_home__slide__pag2__image_pag2'>
+						<div className='contenedor__image_pag2'>
 
-							<div className='slide_home__slide__pag2__image_pag2__image'>
+							<div className='contenedor__image_pag2__image'>
 								<img src="https://licorne-web.s3.us-east-2.amazonaws.com/home/consultorias.svg" alt=""/>
 							</div>
 
 						</div>
-						<div className='slide_home__slide__pag2__imageDos_pag2'>
+						<div className='contenedor__imageDos_pag2'>
 
-							<div className='slide_home__slide__pag2__imageDos_pag2__image'>
+							<div className='contenedor__imageDos_pag2__image'>
 								<img src="https://licorne-web.s3.us-east-2.amazonaws.com/home/tecno.svg" alt=""/>
 							</div>
 
@@ -135,17 +134,17 @@ const slide: NextPage = () => {
 				</div>
 
 				<div className='slide_home__slide__pag3'>
-					<div className='container'>
+					<div className='contenedor container'>
 
-						<div className='slide_home__slide__pag3__image_pag3'>
+						<div className='contenedor__image_pag3'>
 
-							<div className='slide_home__slide__pag3__image_pag3__image'>
+							<div className='contenedor__image_pag3__image'>
 								<img src="https://licorne-web.s3.us-east-2.amazonaws.com/home/tecno.svg" alt=""/>
 							</div>
 
 						</div>
 
-						<div className='slide_home__slide__pag3__text_pag3'>
+						<div className='contenedor__text_pag3'>
 							<h3>TECNOLOGÍA</h3>
 							<p>¿Buscas desarrollo de aplicaciones, ecommerce o sitios web para tu MVP? Estás en el sitio correcto a la hora exacta.</p>
 						</div>
@@ -155,22 +154,22 @@ const slide: NextPage = () => {
 
 				<div className='slide_home__slide__pag4'>
 
-					<div className='container'>
+					<div className='contenedor container'>
 
-						<div className='slide_home__slide__pag4__text_pag4'>
+						<div className='contenedor__text_pag4'>
 							<h3>MARKETING DIGITAL</h3>
 							<p>Más de 10 años en el negocio nos han permitido boostear el inbound y el outbound marketing.</p>
 						</div>
 
-						<div className='slide_home__slide__pag4__textDos_pag4'>
+						<div className='contenedor__textDos_pag4'>
 							<h3>Trabajamos juntos en el <span> problem solution fit </span> para generar tu MVP.
 				Proponemos y probamos <span> hipótesis lean, </span> las validamos con métricas lean
 				para accionar, pivotar y así lograr tracción para llegar al ansiado <span> product market fit. </span></h3>
 						</div>
 
-						<div className='slide_home__slide__pag4__image_pag4'>
+						<div className='contenedor__image_pag4'>
 
-							<div className='slide_home__slide__pag4__image_pag4__image'>
+							<div className='contenedor__image_pag4__image'>
 								<img src="https://licorne-web.s3.us-east-2.amazonaws.com/home/imanes.svg" width='284px' height='326px' alt=""/>
 
 							</div>
@@ -179,10 +178,10 @@ const slide: NextPage = () => {
 					</div>
 				</div>
 
-				<div className='slide_home__slide__pag5'>
-					<div className='container'>
+				<div className='pag5'>
+					<div className='contenedor container'>
 
-						<div className='slide_home__slide__pag5__text_pag5'>
+						<div className='contenedor__text_pag5'>
 							<p>Trabajamos juntos en el <span>problem solution fit</span> para generar tu MVP. Proponemos y probamos <span>hipótesis lean,</span> las validamos con métricas lean para accionar, pivotar y así lograr tracción para llegar al ansiado <span>product market fit. </span></p>
 						</div>
 
@@ -190,7 +189,7 @@ const slide: NextPage = () => {
 
 				</div>
 
-				<div className='slide_home__slide__cubo'></div>
+				<div className='cubo'></div>
 
 			</div>
 		</div>
