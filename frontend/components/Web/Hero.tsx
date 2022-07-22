100b5328078210446e57e7b1299810432d13c0f0
MariaMeza03 <mmeza@licorne.mx>
2022-07-22 13:37:42 -0500

    LICORNE-191

---
+++ frontend/components/Web/Hero.tsx
@@ -0,0 +1,84 @@
+//* Next components
+import type { NextPage } from 'next'
+
+//* Next components
+import Bottle from '../../svg/web/Bottle'
+import BottleTwo from '../../svg/web/BottleTwo'
+import Button from '../../svg/web/Button'
+import Circle from '../../svg/web/Circle'
+import Closing from '../../svg/web/Closing'
+import Cookie from '../../svg/web/Cookie'
+import CookieTwo from '../../svg/web/CookiesTwo'
+
+//*Components
+import Titulo from '../Common/Title'
+
+/**
+ * @author Maria Meza
+ * @name FrontPage
+ * @description Portada Web
+ */
+
+const Hero: NextPage = () => {
+
+	return (
+
+		<div className='hero'>
+			<div className='container'>
+
+				<Titulo 
+					alternativo=""
+					superior="Diseño" 
+					inferior="Web"
+					invertido={false}
+					
+				/>
+				<div className="hero__figCirculo"></div>
+				<div className="hero__imgCirculo">
+					<Circle/>
+				</div>
+
+				<div className="hero__uiux">
+					<div className="hero__uiux__izquierda">
+						<div className="hero__uiux__arriba">
+							<h2>UI</h2>
+						</div>
+						<div className="hero__uiux__abajo">
+							<div className='hero__uiux__abajo__boton'>
+								<Button/>
+							</div>
+							<div className='hero__uiux__abajo__botellas'>
+								<Bottle/>
+							</div>
+							
+							<div className="hero__uiux__abajo__galletas__img">
+								<Cookie/>
+							</div>
+						</div>
+					</div>
+					<div className="hero__uiux__derecha">
+						<div className="hero__uiux__arriba">
+							<h1>UX</h1>
+						</div>
+						<div className="hero__uiux__abajo">
+							<div className='hero__uiux__abajo__botonDos'>
+								<Closing/>
+							</div>
+
+							<div className='hero__uiux__abajo__botellasDos'>
+								<BottleTwo/>
+							</div>
+							
+							<div className="hero__uiux__abajo__galletas__imgDos">
+								<CookieTwo/>
+							</div>
+						</div>
+					</div>
+				</div> 
+			</div>
+		</div>
+
+	)
+}
+
+export default Hero
---
+++ frontend/pages/sitio-web.tsx
@@ -0,0 +1,40 @@
+//* Next components
+import type { NextPage } from 'next'
+
+//*Animation
+import { gsap } from 'gsap/dist/gsap'
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger)
+
+//* Custom Hooks
+import { useViewheight } from '../hooks/useViewheight'
+
+//* Components
+import MainLayout from '../layouts/MainLayout'
+import Hero from '../components/Web/Hero'
+
+/**
+ * @author Maria Meza
+ * @name Web
+ * @description Web
+ */
+
+const Web: NextPage = () => {
+
+	const title = 'Diseño Web UI/UX Usamos tecnologías seguras y dinámicas'
+	const description = 'Elaboramos diseños estratégicos que permitan destacar de la competencia a través de una experiencia increíble. ¿Qué esperas para tener un sitio web único?'
+
+	useViewheight()
+
+	return (
+
+		<MainLayout title={title} description={description}>
+			<Hero/>
+		
+		</MainLayout>
+
+	)
+
+}
+
+export default Web
