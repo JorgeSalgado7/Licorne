6adbe3e316cdff7c865baa0fe4385184c31d4c5c
MariaMeza03 <mmeza@licorne.mx>
2022-07-26 12:44:18 -0500

    LICORNE-203

---
+++ frontend/pages/app.tsx
@@ -0,0 +1,40 @@
+//* Next components
+import type { NextPage } from 'next'
+
+//* Custom Hooks
+import { useViewheight } from '../hooks/useViewheight'
+
+//* Components
+import MainLayout from '../layouts/MainLayout'
+import Hero from '../components/App/Hero'
+import Methodology from '../components/App/Methodology'
+import Requirements from '../components/App/Requirements'
+import WeKnow from '../components/App/WeKnow'
+
+/**
+ * @author Maria Meza 
+ * @name App
+ * @description Página principal del sitio web
+ */
+
+const App: NextPage = () => {
+
+	const title = 'Desarrollo de Apps: Swift, Flutter, React Native, Cordova.'
+	const description = 'Conéctate digitalmente con apps que ofrecen la mejor experiencia de usuario. Fáciles de entender y usar. Compatibles con App Store y Play Store.'
+
+	useViewheight()
+
+	return (
+
+		<MainLayout title={title} description={description}>
+			<Hero/>
+			<Methodology/>
+			<Requirements/>
+			<WeKnow/>
+		</MainLayout>
+
+	)
+
+}
+
+export default App
---
+++ frontend/svg/App/Hand.tsx
@@ -0,0 +1,27 @@
+//* Next components
+import type { NextPage } from 'next'
+
+/**
+ * @author Maria Meza
+ * @name SVG_Insight
+ * @description Imagen SVG
+ */
+
+const Hand: NextPage = () => {
+
+	return (
+		<svg id="Capa_2" data-name="Capa 2" xmlns="http://www.w3.org/2000/svg" width="533.47" height="455.55" viewBox="0 0 533.47 455.55">
+			<g id="Capa_1" data-name="Capa 1">
+				<path id="Trazado_5" data-name="Trazado 5" d="M237.27,256.1,174.9,290.6,0,115.06,24.02,41.24Z" fill="#ffdf17"/>
+				<path id="Trazado_6" data-name="Trazado 6" d="M275.02,356.86l47.6,78.76L96.61,395.18,73.33,321.13Z" fill="#ffdf17"/>
+				<path id="Trazado_7" data-name="Trazado 7" d="M273.16,179.87l-36.38,61.95L93.32,102.58l22.99-74.14Z" fill="#ffdf17"/>
+				<path id="Trazado_8" data-name="Trazado 8" d="M332.09,130.6l-56.26,40.61L185.62,81.56,208.61,7.43Z" fill="#ffdf17"/>
+				<path id="Trazado_9" data-name="Trazado 9" d="M425.35,113.54,381.6,168.9,287.2,74.14,310.2,0Z" fill="#ffdf17"/>
+				<path id="Trazado_10" data-name="Trazado 10" d="M327.22,453.55a179.28,179.28,0,0,1-129.53-88.36l93.7.44A108.78,108.78,0,0,0,337.71,384c59.44,8.85,114.85-31.85,123.76-90.92S429.41,179,370,170.1,255.12,202,246.21,261q-.65,4.26-.94,8.47l-70.54,21.41a177.724,177.724,0,0,1,1.48-40.31c14.7-97.48,106.15-164.66,204.25-150s165.71,105.49,151,203S425.32,468.17,327.22,453.55Z" fill="#ffdf17"/>
+				<path id="Trazado_11" data-name="Trazado 11" d="M280.47,357.16s3.49,3,4.82,4l6.1,4.49-80.69,3.92,51.79-14.73Z" fill="#ffdf17"/>
+			</g>
+		</svg>
+	)
+
+}
+export default Hand
