9f66f40c734e115b5a11d4f035d967f00468a2b8
JorgeSalgado7 <j.salgado7@outlook.com>
2022-07-29 21:57:59 -0500

    Correcciones lean

--- frontend/components/LeanStartup/SnapAnimation.tsx
+++ frontend/components/LeanStartup/SnapAnimation.tsx
@@ -2,9 +2,12 @@
 import type { NextPage } from 'next'
 
 //* SVG
+import Hand from '../../svg/LeanStartup/Hand'
 import Snap from '../../svg/LeanStartup/Snap'
 import Finger from '../../svg/LeanStartup/Finger'
-import Hand from '../../svg/LeanStartup/Hand'
+
+//* Custom hooks
+import { useViewheight } from '../../hooks/useViewheight'
 
 /**
  * @author Iván Sánchez
@@ -13,35 +16,27 @@ import Hand from '../../svg/LeanStartup/Hand'
  */
 const SnapAnimation: NextPage = () => {
 
+	useViewheight()
+
 	return (
 
 		<div className='snap_animation'>
-			<div className='snap_animation__container container'>
-				<div className='snap_animation__container__text'>
+
+			<div className='container'>
+
+				<div className='snap_animation__text'>
 					<h3>Los cracks de la metodología lean startup </h3>
 					<h3>¡Están aquí!</h3>
 				</div>
-				<div className='snap_animation__container__animation'>
-					<div className="snap_animation__container__animation__hand">
 
-						<div className='snap_animation__container__animation__hand__image'>
-							<Hand />
-						</div>
+				<div className='snap_animation__animation'>
 
+					<div className='snap_animation__animation__hand'>
+						<Hand />
 					</div>
-					<div className="snap_animation__container__animation__thumb">
-						<div className='snap_animation__container__animation__thumb__image'>
-							<Finger />
-						</div>
 
-					</div>
-					<div className="snap_animation__container__animation__index"></div>
-					<div className="snap_animation__container__animation__snap">
-						<div className='snap_animation__container__animation__snap__image'>
-							<Snap />
-						</div>
-					</div>
 				</div>
+
 			</div>
 		</div>
 
